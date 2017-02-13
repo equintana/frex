61067d6443efd59c612efc5f0cc9f4ccdf948631
Erlinis Quintana <erlinis.quintana@koombea.com>
2017-02-13 14:33:33 -0500

    Declaring and using props

--- src/index.js
+++ src/index.js
@@ -1,40 +1,47 @@
 /*
- * Second example, using JSX to create custom components
+ * Third example, declaring and using components props.
  */
 
 import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom';
 
-class MyFirstComponent extends Component {
+var items = [
+  { name: "David",
+    age: 30
+  },
+  { name: "Erlinis",
+    age: 30
+  },
+  { name: "Peter",
+    age: 31
+  }
+]
+
+
+class List extends Component {
  render(){
-   return <span>{this.props.title}</span>;
+   return (
+     <ul>
+     {this.props.children}
+     </ul>
+   )
  }
 }
 
-MyFirstComponent.propTypes = {
-  title: PropTypes.string.isRequired
+class ListItem extends Component {
+  render(){
+   return <li>{this.props.name}, age:  {this.props.age} </li>
+  }
 }
 
-MyFirstComponent.defaultProps = {
-  title: ' defaults '
+ListItem.propTypes = {
+  name: PropTypes.string.isRequired
 }
 
-
-function MySecondComponent( props ){
-  return <span>{props.title}</span>;
-}
-
-MySecondComponent.propTypes = {
-  title: PropTypes.string
-}
 
 render(
-  <h1 className='hola'>
-     1er
-     <span>2do </span>
-     <MyFirstComponent title="title first class... "></MyFirstComponent>
-     <MySecondComponent title="title second function... "></MySecondComponent>
-  </h1>
+  <List>
+     { items.map( item => <ListItem key={item.name} name={item.name} age={item.age}></ListItem>) }
+  </List>
   ,document.getElementById('root')
-);
-
+)
